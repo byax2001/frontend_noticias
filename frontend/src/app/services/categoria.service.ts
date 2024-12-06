52dc38673f4ce129ac6dd0c7022377c6845f7723
Brandon Yax <3043441920114@ingenieria.usac.edu.gt>
2024-12-06 16:48:59 -0600

    [ADD] JWT Funcionality
    Se agrego la funcionalidad JWT para la realización de peticiones seguras

--- frontend/src/app/services/categoria.service.ts
+++ frontend/src/app/services/categoria.service.ts
@@ -4,10 +4,11 @@ import { Injectable } from '@angular/core';
 @Injectable({providedIn: 'root'})
 export class CategoriaService {
   url = 'http://localhost:8080/categorias';
+  token = localStorage.getItem('token');
   constructor(private http: HttpClient) { }
 
   getCategorias() {
-    return this.http.get(`${this.url}`);
+    return this.http.get(`${this.url}`, {headers: {Authorization: `Bearer ${this.token}`}});
   }
 
 
